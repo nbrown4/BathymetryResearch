78b2e75daf411de81fbfd3ad93ef8f7070d377ca
tortdagreat <davidcroft@live.com>
2023-02-10 17:02:35 -0500

    Flake8Views

--- Website/BathyProject/Website/views.py
+++ Website/BathyProject/Website/views.py
@@ -1,21 +1,17 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 import os
-import numpy as np
-import pandas as pd
 import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt 
-
+import matplotlib.pyplot
 from .FullScript import sub80samples
 from .FullScript import Script_3000
+matplotlib.use('Agg')
+
 path = os.getcwd() + "\\Website\\static\\DataFiles"
-
 entries = os.listdir(path)
+views = Blueprint('views', __name__)
 
 
-views = Blueprint('views',__name__)
-
-@views.route("/BathymetryML/", defaults={'filename':None, 'filename2':None}, methods=['GET', 'POST'])
+@views.route("/BathymetryML/", defaults={'filename': None, 'filename2': None}, methods=['GET', 'POST'])
 @views.route("/BathymetryML/<filename>/<filename2>", methods=['GET', 'POST'])
 def BathymetryML(filename, filename2):
     if request.method == 'POST':
@@ -30,9 +26,11 @@ def BathymetryML(filename, filename2):
 
     return render_template("BathymetryML.html")
 
+
 @views.route("/dragAndDrop/", methods=['GET', 'POST'])
 def dragAndDrop():
     return redirect(url_for("views.home"))
+
 
 @views.route("/", methods=['GET', 'POST'])
 def home():
@@ -43,26 +41,27 @@ def home():
             print(userInput)
             return redirect(url_for("views.BathymetryML", filename=userInput, filename2=userOutput))
 
-    return render_template("dragAndDrop.html", len = len(entries), entries = entries)
+    return render_template("dragAndDrop.html", len=len(entries), entries=entries)
 
 
 @views.route("/testPage/", methods=['GET', 'POST'])
-def testPage(): 
+def testPage():
     if request.method == 'POST':
         for i in request.form:
             if i == "loadAnimation":
-                return render_template("testPage.html", a = "../static/BrainAnimationVid0001-0125.mp4")
-                
+                return render_template("testPage.html", a="../static/BrainAnimationVid0001-0125.mp4")
+
     return render_template("testPage.html")
+
 
 @views.route("/outputPage/", methods=['GET', 'POST'])
 def outputPage():
     return render_template("outputPage.html")
 
-def machine(filename,filename2):
+
+def machine(filename, filename2):
     dfMain = sub80samples(filename)
     dfMain = dfMain['sample']
     dfMain.to_csv('Website/static/CleanData/clean80.csv')
     DF_3000 = Script_3000(filename2)
     DF_3000.to_csv('Website/static/CleanData/DF_3000.csv')
-
